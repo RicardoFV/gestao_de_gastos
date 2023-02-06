fb1251acd9626299b2b4856b2955c1148d3e37f6
Ricardo Ferreira <ricardo.ti.ads@gmail.com>
2023-02-05 22:12:41 -0300

    criando a parte do frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NbThemeModule } from '@nebular/theme';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/template/header/header.component';
+import { FooterComponent } from './componentes/template/footer/footer.component';
+import { LoginComponent } from './componentes/views/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
